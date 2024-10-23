17e4a5f97e30efd31e61e1d1cc128ff22526414b
SEDKI23 <151510415+SEDKI23@users.noreply.github.com>
2024-10-23 20:07:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ git config --global -add safe.directory /github/workspace
 python3 /usr/bin/feed.py 
 
 git add -A && commit -m "Update Feed"
-git push --set-upstrea origin main
+git push --set-upstream origin main
 
 echo "====================="
